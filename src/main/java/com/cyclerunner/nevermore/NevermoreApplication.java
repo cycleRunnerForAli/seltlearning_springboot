129e1449100bb506368468413188611254213a8a
cycleRunnerForAli <791415030@qq.com>
2019-07-11 21:38:38 +0800

    init repo

---
+++ src/main/java/com/cyclerunner/nevermore/NevermoreApplication.java
@@ -0,0 +1,13 @@
+package com.cyclerunner.nevermore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevermoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NevermoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyclerunner/nevermore/NevermoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cyclerunner.nevermore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NevermoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
