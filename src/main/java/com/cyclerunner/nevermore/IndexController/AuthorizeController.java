56d667ede55c6b824592476b663aa4161b6f6dcf
cycleRunnerForAli <791415030@qq.com>
2019-07-22 23:50:01 +0800

    add GitHubProvider

--- src/main/java/com/cyclerunner/nevermore/IndexController/AuthorizeController.java
+++ src/main/java/com/cyclerunner/nevermore/IndexController/AuthorizeController.java
@@ -1,5 +1,9 @@
 package com.cyclerunner.nevermore.IndexController;
 
+import com.cyclerunner.nevermore.dto.AccessTokenDTO;
+import com.cyclerunner.nevermore.dto.GitHubUser;
+import com.cyclerunner.nevermore.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,10 +11,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthorizeController {
 
+    @Autowired
+    GithubProvider githubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
-
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.5602739ed0ac98c9");
+        accessTokenDTO.setClient_secret("31a307410778ddf39600cdf06d26f1100bff0718");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GitHubUser gitHubUser = githubProvider.getGitHubUser(accessToken);
+        System.out.println(gitHubUser.getName());
         return "index";
     }
 }
--- src/main/java/com/cyclerunner/nevermore/provider/GithubProvider.java
+++ src/main/java/com/cyclerunner/nevermore/provider/GithubProvider.java
@@ -2,6 +2,7 @@ package com.cyclerunner.nevermore.provider;
 
 import com.alibaba.fastjson.JSON;
 import com.cyclerunner.nevermore.dto.AccessTokenDTO;
+import com.cyclerunner.nevermore.dto.GitHubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -20,9 +21,28 @@ public class GithubProvider {
                 .url("https://github.com/login/oauth/access_token")
                 .post(body).build();
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+            String accessToken = response.body().string();
+            System.out.println(accessToken.split("&")[0].split("=")[1]);
+            return accessToken.split("&")[0].split("=")[1];
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GitHubUser getGitHubUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+
+            String string = response.body().string();
+            System.out.println(string);
+            return JSON.parseObject(string, GitHubUser.class);
+        } catch (IOException e) {
         }
         return null;
     }
