db0bdee78f2bf454168b1ebf7085b5109bd27f22
cycleRunnerForAli <791415030@qq.com>
2019-08-06 21:40:54 +0800

    add_AuthorizeController_step 1

--- src/main/java/com/cyclerunner/nevermore/IndexController/AuthorizeController.java
+++ src/main/java/com/cyclerunner/nevermore/IndexController/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -24,7 +26,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -33,7 +36,11 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(clientRedirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GitHubUser gitHubUser = githubProvider.getGitHubUser(accessToken);
-        System.out.println(gitHubUser.getName());
-        return "index";
+        if (gitHubUser != null) {
+            request.getSession().setAttribute("user", gitHubUser);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
     }
 }
